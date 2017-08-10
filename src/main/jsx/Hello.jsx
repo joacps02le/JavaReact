b3521049bfac468948b4724d8facd1a3a61d8404
joacps02le <jo.acps02le>
2017-08-10 14:03:26 +0200

    create component

--- src/main/jsx/Hello.jsx
+++ src/main/jsx/Hello.jsx
@@ -2,12 +2,20 @@ import React from 'react'
 
 import './Hello.less'
 
-const Hello = React.createClass({
-    render: function() {
-        return (
-            <div className="HelloWorld">Hello {this.props.who}</div>
-        )
-    }
-})
+/** const Hello = React.createClass({
+   * render: function() {
+   *     return (
+   *         <div className="HelloWorld">Hello {this.props.who}</div>
+   *     )
+   * }
+}) */
 
+class Hello extends React.Component{
+	render(){
+		return(
+			<div className="HelloWorld">hola {this.props.who}</div>
+		);
+	}
+	
+}
 export default Hello
